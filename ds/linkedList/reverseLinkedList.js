1bf7a5ea835fa90147aebc863fe7bb85fd8bd893
Adil <adilc0070@gmail.com>
2024-03-09 15:52:50 +0530

    LL reverse

---
+++ ds/linkedList/reverseLinkedList.js
@@ -0,0 +1,70 @@
+class Node{
+    constructor(value){
+        this.value=value
+        this.next=null
+    }
+}
+class LL{
+    constructor(){
+        this.head=null
+        this.size=0
+    }
+    isEmpty(){
+        return this.size==0
+    }
+    getSize(){
+        return this.size
+    }
+    prepend(value){
+        let newNode=new Node(value)
+        if(this.isEmpty()){
+            this.head=newNode
+        }else{
+            newNode.next=this.head
+            this.head=newNode
+        }
+        this.size++
+    }
+    apend(value){
+        let newNode=new Node(value)
+        if(this.isEmpty()){
+            this.head=newNode
+        }else{
+            let curr=this.head
+            while(curr.next){
+                curr=curr.next
+            }
+            curr.next=newNode
+        }
+        this.size++
+    }
+    reverse(){
+        let prev=null
+        let curr = this.head
+        while(curr){
+            let next=curr.next
+            curr.next=prev
+            prev=curr
+            curr=next
+        }
+        this.head=prev
+    }
+    print(){
+        let listItem=[]
+        let curr=this.head
+        while(curr){
+            listItem.push(curr.value)
+            curr=curr.next
+        }
+        console.log(listItem.join('->'));
+    }
+}
+
+
+let linkedList=new LL()
+linkedList.prepend(19)
+linkedList.prepend(10)
+linkedList.apend(11)
+linkedList.apend(12)
+linkedList.reverse()
+linkedList.print()
