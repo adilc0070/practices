f55014430413c833872f45a7c19e97d31a77624d
Adil <adilc0070@gmail.com>
2024-04-04 14:16:03 +0530

    pendings added and explaination

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,1 +1,41 @@
-adil
+// function hellow(name,greet='welco',age){
+//     console.log(`${greet} ${name} your age is ${age}`)
+// }
+// let a
+// hellow('adil',a,20)
+
+// let obj={
+//     name:'adil',
+//     age:20,
+//     place:{
+//         city:'malappuram',
+//         state:'kerala'
+//     }
+// }
+// let a=Object.toString(obj)
+// console.log(...a);
+
+// Example of an object with option methods
+// const MyLibrary = {
+//     options: {
+//       debug: false,
+//       timeout: 3000
+//     },
+  
+//     setOption(key, value) {
+//       this.options[key] = value;
+//     },
+  
+//     getOption(key) {
+//       return this.options[key];
+//     }
+//   };
+  
+//   // Using option methods
+//   MyLibrary.setOption('debug', true);
+//   console.log(MyLibrary.getOption('debug')); // Outputs: true
+//   console.log(MyLibrary.getOption('debug')); // Outputs: true
+// //   console.log(MyLibrary.getOption('timeout')); // Outputs: 3000
+  
+// console.log(3+5+'7');
+isNan
